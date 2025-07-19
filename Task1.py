7c8fa3c6f1c73459560803c774a464032d099fae
Chetna Patel <99310118+Chetna100@users.noreply.github.com>
2025-07-19 21:19:13 +0530

    Submission of Task 1
    This is my Task one submission.

---
+++ Task1.py
@@ -0,0 +1,15 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+c= a+b
+c=int(c)
+print("Addition:", c)
+
+d = (a-b)
+d=int(d)
+print("Subtraction:", d)
+
+e = a*b
+print("Multiplication:", e)
+f = (a/b)
+f=int(f)
+print("Division:", f)
